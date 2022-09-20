095edd8e5bea83a196810038602dad171b63d14e
camilacosta <camilavitoria.c.costa@gmail.com>
2022-09-20 15:22:33 -0400

    bootstrap + http commons + PokemonDataService

---
+++ src/services/PokemonDataService.js
@@ -0,0 +1,44 @@
+import http from '../http-commons'
+
+
+class PokemonDataService {
+    async buscarTodos() {
+        let resposta = await http.get('/pokemons');
+        return resposta.data;
+    }
+
+    async buscarPorTermo(termo) {
+        let resposta = await http.get('/pokemons?termo=${termo}');
+        return resposta.data;
+    }
+
+    async buscarPorId(id) {
+        let resposta = await http.get('/pokemons/${id}');
+        return resposta.data;
+    }
+
+    async buscarPeloIdDoTipo(id) {
+        let resposta = await http.get('/pokemons/tipo/${id}');
+        return resposta.data;
+    }
+
+    async criar(pokemon) {
+        let resposta = await http.post('/pokemons', pokemon);
+        return resposta.data;
+    }
+
+    async atualizar(pokemon, id) {
+        let resposta = await http.put('/pokemons/${id}', pokemon);
+        return resposta.data;
+    }
+
+    async removerPorId(id) {
+        await http.delete('/pokemons/${id}');
+    }
+
+    async removerPorNome(termo) {
+        await http.delete('/pokemons?termo=${termo}');
+    }
+}
+
+export default new PokemonDataService();
