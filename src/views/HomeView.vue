622bb598bfc3a16a3ace458456135f7940e41023
camilacosta <camilavitoria.c.costa@gmail.com>
2022-09-21 13:07:10 -0400

    estilização lista

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,13 +25,27 @@ export default {
 <template>
   <div>
     <h2>Lista de Pokemons</h2>
-    <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">
-        <img
-          :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemon.numeroPokedex +'.png'"
-          alt="imagem pokemon" />
-        {{pokemon.numeroPokedex}} - {{pokemon.nome}}
-      </li>
-    </ul>
+    <div class="container-lg text-center row">
+        <div class="col-md-auto" v-for="pokemon in pokemons" :key="pokemon.id">
+          <div class="card mb-4" style="max-width: 240px;">
+            <h5 class="card-header">
+              <img
+                :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ pokemon.numeroPokedex +'.png'"
+                alt="imagem pokemon" />
+              Nº.{{pokemon.numeroPokedex}}
+            </h5>
+            <div class="card-body">
+
+              <div class="col">
+                <h5 class="card-title">{{pokemon.nome}}</h5>
+                <img class="img-fluid rounded-start" style="max-width: 140px;"
+                  :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/'+ pokemon.numeroPokedex +'.png'"
+                  alt="imagem pokemon" />
+                <p class="card-text"> Nível: {{pokemon.nivel}} </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
