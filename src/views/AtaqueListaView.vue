f7e9e0fd36383467cd80144cc2233a2f0c6b2103
camilacosta <camilavitoria.c.costa@gmail.com>
2022-09-29 12:57:48 -0400

    Adicionando modal com confirmação de exclusão

--- src/views/AtaqueListaView.vue
+++ src/views/AtaqueListaView.vue
@@ -3,7 +3,10 @@ import AtaqueDataService from '../services/AtaqueDataService';
 export default {
     name: "ataques-lista",
     data() {
-        return { ataques: [] };
+        return {
+            ataques: [],
+            idSelecionado: null,
+        }
     },
     methods: {
         buscarTipos() {
@@ -20,7 +23,9 @@ export default {
             this.$router.push({ name: 'ataques-editar', params: { id: id } });
         },
 
-        remover(id) {
+        remover() {
+
+            const id = this.idSelecionado;
             AtaqueDataService.removerPorId(id)
                 .catch(erro => {
                     console.log(erro);
@@ -28,8 +33,15 @@ export default {
 
             let i = this.ataques.map(item => item.id).indexOf(id) // find index of your object
             this.ataques.splice(i, 1) // remove it from array
+
+            this.idSelecionado = null;
+        },
+
+        selecionarIdAtaque(id) {
+            this.idSelecionado = id;
         }
     },
+
     mounted() {
         this.buscarTipos();
     }
@@ -66,25 +78,49 @@ export default {
                         <td>{{ataque.categoria}}</td>
                         <td>{{ataque.tipo.nome}}</td>
                         <td>
-                            <button @click="editar(ataque.id)" class="btn btn-dark">
+                            <button @click="editar(ataque.id)" class="btn btn-outline-dark">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                     <path
-                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path fill-rule="evenodd"
-                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                                 </svg>
-                                Editar
                             </button>
                         </td>
                         <td>
-                            <button @click="remover(ataque.id)" class="btn btn-dark">
-                                Remover
+                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
+                                data-bs-target="#modalExcluir" @click="selecionarIdAtaque(ataque.id)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                </svg>
                             </button>
+
                         </td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="modalExcluir" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Deseja excluir o ataque com o id: {{idSelecionado}}?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button @click="remover()" class="btn btn-dark" data-bs-dismiss="modal">
+                        Remover
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
