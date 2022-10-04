cee202e2402e69d7b1e8a249ff400697bbd80b0d
camilacosta <camilavitoria.c.costa@gmail.com>
2022-10-04 14:13:43 -0400

    collapse mais informações pokemon

--- src/views/pokemon/PokemonListaView.vue
+++ src/views/pokemon/PokemonListaView.vue
@@ -44,7 +44,7 @@ export default {
                 alt="imagem pokemon" />
             </div>
             <div class="mt-2">
-              <button @click="editar(pokemon.id)" class="m-1 btn btn-outline-primary">
+              <button class="m-1 btn btn-outline-primary" data-bs-toggle="collapse" :data-bs-target="'#collapse'+ pokemon.id">
                 Mais
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg"
                   viewBox="0 0 16 16">
@@ -67,6 +67,14 @@ export default {
                     d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                 </svg>
               </button>
+
+              <div class="collapse" :id="'collapse'+ pokemon.id">
+                <div class="card card-body">
+                  <p class="card-text"> Peso: {{pokemon.peso}} </p>
+                  <p class="card-text"> Altura: {{pokemon.altura}} </p>
+                  <p class="card-text"> Felicidade: {{pokemon.felicidade}} </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
