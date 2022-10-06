d04c60c81b021c5b920b765a2e2f083fe62ac238
camilacosta <camilavitoria.c.costa@gmail.com>
2022-10-06 16:43:30 -0400

    endereço - treinador formulario

--- src/views/treinador/TreinadorNovoView.vue
+++ src/views/treinador/TreinadorNovoView.vue
@@ -89,30 +89,33 @@ export default {
                             </option>
                         </select>
                     </div>
-                    <div class="row">
-                        <div class="col-4" v-for="endereco in enderecos" :key="endereco.id">
-                            <input type="radio" class="btn-check" name="options" :id="endereco.id" autocomplete="off">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-3" v-for="endereco in enderecos" :key="endereco.id">
+                                <input type="radio" class="btn-check" name="options" :id="endereco.id"
+                                    autocomplete="off">
 
-                            <label class="btn" :for="endereco.id">
-                                <div class="card h-100" style="width:200px">
-                                    <div class="card-body">
-                                        <p class="card-text">Cidade: {{endereco.cidade}}</p>
-                                        <p class="card-text">Regiao: {{endereco.regiao}}</p>
+                                <label class="btn" :for="endereco.id">
+                                    <div class="card" style="width:200px; height:150px;">
+                                        <div class="card-body">
+                                            <p class="card-text">Cidade: {{endereco.cidade}}</p>
+                                            <p class="card-text">Regiao: {{endereco.regiao}}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </label>
+                                </label>
 
-                        </div>
-                        <div class="col-4">
-                            <div class="card align-items-center h-100" style="width:200px">
-                                <button type="button" class="btn btn-outline-dark h-100 w-100" data-bs-toggle="modal"
-                                    data-bs-target="#enderecoNovoModal">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-plus-lg" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
-                                    </svg>
-                                </button>
+                            </div>
+                            <div class="col-2">
+                                <div class="card align-items-center" style="width:200px; height:150px;">
+                                    <button type="button" class="btn btn-outline-dark h-100 w-100"
+                                        data-bs-toggle="modal" data-bs-target="#enderecoNovoModal">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
